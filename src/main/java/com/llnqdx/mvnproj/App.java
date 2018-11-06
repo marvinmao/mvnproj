e78ef6b572b54c44f331c3fcbc21d2995699dcb3
maofujiang <1512355855@qq.com>
2018-11-06 22:06:30 +0800

    init project import

---
+++ src/main/java/com/llnqdx/mvnproj/App.java
@@ -0,0 +1,13 @@
+package com.llnqdx.mvnproj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
