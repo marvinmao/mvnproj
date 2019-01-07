a289e3100dc6f6f1860a49d689794b3a13cf2cd7
maofujiang <1512355855@qq.com>
2019-01-07 21:21:30 +0800

    config

---
+++ mvnproj-service/src/main/java/com/llnqdx/mvnproj/springaop/security/SecurityAspect.java
@@ -0,0 +1,9 @@
+package com.llnqdx.mvnproj.springaop.security;
+
+/**
+ * @Auther: marvinmao
+ * @Date: 2019/1/6
+ * @Description:
+ */
+public class SecurityAspect {
+}
---
+++ mvnproj-service/src/main/java/com/llnqdx/mvnproj/springaop/service/AuthService.java
@@ -0,0 +1,9 @@
+package com.llnqdx.mvnproj.springaop.service;
+
+/**
+ * @Auther: marvinmao
+ * @Date: 2019/1/6
+ * @Description:
+ */
+public class AuthService {
+}
