6140f729fe125d5c5ea354cd35d1f7f125b48fb6
maofujiang <1512355855@qq.com>
2019-07-08 22:27:22 +0800

    spring-anno

---
+++ spring-anno/src/main/java/com/enjoy/cap6/config/JamesFactoryBean.java
@@ -0,0 +1,24 @@
+package com.enjoy.cap6.config;
+
+import com.enjoy.cap6.bean.Monkey;
+import org.springframework.beans.factory.FactoryBean;
+
+public class JamesFactoryBean implements FactoryBean<Monkey> {
+
+    @Override
+    public Monkey getObject() throws Exception {
+        // TODO Auto-generated method stub
+        return new Monkey();
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        // TODO Auto-generated method stub
+        return Monkey.class;
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return true;
+    }
+}
